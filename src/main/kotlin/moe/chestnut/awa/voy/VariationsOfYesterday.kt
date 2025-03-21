17295968dfc8f20daf18fee14d0809ec9e7f4a0e
GGGUA7 <2429315109@qq.com>
2025-03-21 22:29:10 +0800

    增加了一个功能

--- src/main/kotlin/moe/chestnut/awa/voy/VariationsOfYesterday.kt
+++ src/main/kotlin/moe/chestnut/awa/voy/VariationsOfYesterday.kt
@@ -2,20 +2,18 @@ package moe.chestnut.awa.voy
 
 import moe.chestnut.awa.voy.herbert.HerbertItems
 import net.fabricmc.api.ModInitializer
-import org.slf4j.LoggerFactory
+import org.slf4j.LoggerFactory import javax.naming.directory.ModificationItem
 
 /**
  * Variations of Yesterday: Lie No.13 in Serenity
  * */
 object VariationsOfYesterday : ModInitializer {
-    private val logger = LoggerFactory.getLogger("variations-of-yesterday")
+	private val logger = LoggerFactory.getLogger( "variations-of-yesterday")
 
 	override fun onInitialize() {
 		HerbertItems.initialize()
 
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+
 		logger.info("Variations of Yesterday initialized!")
 	}
 }
