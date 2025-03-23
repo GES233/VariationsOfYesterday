d58fed3d6cce9c95b7f74b0d403e3be08f46744e
RoastedChestnut <chestnut.roasted@outlook.com>
2025-03-23 19:19:14 +0800

    [VoY][plant] Add LuminousGrass

--- src/main/scala/moe/chestnut/awa/voy/VariationsOfYesterday.scala
+++ src/main/scala/moe/chestnut/awa/voy/VariationsOfYesterday.scala
@@ -1,15 +1,18 @@
 package moe.chestnut.awa.voy
 
-import moe.chestnut.awa.voy.herbert.HerbertItems
-import moe.chestnut.awa.voy.plant.PlantBlocks
+import moe.chestnut.awa.voy.plant.{PlantBlocks, PlantItems}
 import net.fabricmc.api.ModInitializer
 import org.slf4j.{Logger, LoggerFactory}
 
+/**
+ * *Variations of Yesterday: Lie No.13 in Serenity*
+ * is a Minecraft mod with no meaning or purpose.
+**/
 object VariationsOfYesterday extends ModInitializer:
   private val logger: Logger = LoggerFactory.getLogger(getClass)
 
   override def onInitialize(): Unit =
     PlantBlocks.initialize()
-    HerbertItems.initialize()
+    PlantItems.initialize()
 
     logger.info("Variations of Yesterday with Scala initialized!")
--- src/main/scala/moe/chestnut/awa/voy/plant/PlantItems.scala
+++ src/main/scala/moe/chestnut/awa/voy/plant/PlantItems.scala
@@ -1,15 +1,14 @@
-package moe.chestnut.awa.voy.herbert
+package moe.chestnut.awa.voy.plant
 
 import net.minecraft.item.Item
 import net.minecraft.registry.{RegistryKey, RegistryKeys, Registry, Registries}
 import net.minecraft.util.Identifier
 
-object HerbertItems {
+object PlantItems {
   def initialize(): Unit = { }
 
   // LuminousGrass
   private val LUMINOUS_GRASS_LEAVES: Item = register("luminous_grass_leaves", settings => Item(settings), Item.Settings())
-  private val LUMINOUS_GRASS_STEM: Item = register("luminous_grass_stem", settings => Item(settings), Item.Settings())
   
   private def register(path: String, factory: Item.Settings => Item, settings: Item.Settings): Item = {
     val itemKey: RegistryKey[Item] = RegistryKey.of(RegistryKeys.ITEM, Identifier.of("voy", path))
