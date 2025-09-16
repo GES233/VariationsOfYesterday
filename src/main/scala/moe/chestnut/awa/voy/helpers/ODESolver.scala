91ee2ac80dcb0d5eaa40819150ed54fc750df772
RoastChestnut <30802664+GES233@users.noreply.github.com>
2025-09-16 13:34:19 +0800

    [VoY][mandate] Fix some bug

--- src/main/scala/moe/chestnut/awa/voy/helpers/ODESolver.scala
+++ src/main/scala/moe/chestnut/awa/voy/helpers/ODESolver.scala
@@ -1,39 +1,39 @@
 package moe.chestnut.awa.voy.helpers
 
 trait NumericTuple:
-  def toArray(): Array
-  def fromArray(nums: Array): NumericTuple
+  def toArray: Array[Double]
+  def fromArray(interval_numbers: Array[Double]): NumericTuple
 
-class ODESolver(equation: (Array[Double], Map[String, Any]) => Array[Double]):
+class ODESolver(equation: (Array[Double], Option[Map[String, Any]]) => Array[Double]):
   val useEular: Symbol = Symbol("USE_EULAR")
   val useRK4: Symbol = Symbol("USE_RK4")
 
   def update(
       currentState: NumericTuple,
       dt: Double,
-      method: Symbol = useRK4,
-      extra: Map[String, Any] = Map.empty()
-  ): Array[Double] =
-    val res = method match
-      case useEular => useEular_(currentState.toArray(), dt, extra)
-      case useRK4   => useRK4_(currentState.toArray(), dt, extra)
+      method: Symbol = this.useRK4,
+      extra: Option[Map[String, Any]] = None
+  ): NumericTuple =
+    val res: Array[Double] = method match
+      case this.useEular => useEular_(currentState.toArray, dt, extra)
+      case this.useRK4 => useRK4_(currentState.toArray, dt, extra)
       // case _: Any => new `throws`
 
-    getClass(currentState).fromArray(res)
+    currentState.fromArray(res)
 
-  // ignore t.
+  // ignore absolutely `t`.
   private def useEular_(
       currentState: Array[Double],
       dt: Double,
-      extra: Map[String, Any]
+      extra: Option[Map[String, Any]]
   ): Array[Double] =
     val dot_var = equation(currentState, extra)
-    currentState.zip(dot_var).map { case (yi, dydti) => yi + dt * dydti }
+    currentState.zip(dot_var).map { case (y_i, y_dot_i) => y_i + dt * y_dot_i }
 
   private def useRK4_(
       currentState: Array[Double],
       dt: Double,
-      extra: Map[String, Any]
+      extra: Option[Map[String, Any]]
   ): Array[Double] =
     val k1 = equation(currentState, extra)
     val k2 = equation(
--- src/main/scala/moe/chestnut/awa/voy/inner/mandate/MandateState.scala
+++ src/main/scala/moe/chestnut/awa/voy/inner/mandate/MandateState.scala
@@ -1,22 +1,22 @@
 package moe.chestnut.awa.voy.inner.mandate
 
-import moe.chestnut.awa.voy.helpers.NumericTuple
+import moe.chestnut.awa.voy.helpers.{NumericTuple, ODESolver}
 
 case class MandateParam(alpha: Double, beta: Double, epsilon: Double)
     extends NumericTuple:
-  def toArray(): Array[Double] =
+  def toArray: Array[Double] =
     Array(alpha, beta, epsilon)
 
   def fromArray(nums: Array[Double]): MandateParam =
-    MandateParam(nums[0], nums[1], nums[2])
+    MandateParam(nums(0), nums(1), nums(2))
 
 case class MandateCurrent(dissonance: Double, entrench: Double)
     extends NumericTuple:
-  def toArray(): Array[Double] =
+  def toArray: Array[Double] =
     Array(dissonance, entrench)
 
   def fromArray(nums: Array[Double]): MandateCurrent =
-    MandateCurrent(nums[0], nums[1])
+    MandateCurrent(nums(0), nums(1))
 
 /** Bla bla.
   */
@@ -26,8 +26,25 @@ class MandateState(
 ):
   // TODO: find suitable default params and initial state.
 
-  def update(timestep: Double): Unit = {}
-  // Calculate ...
+  private def do_update(current: Array[Double]): Array[Double] =
+    val delta_dis = 0.0
+    val delta_en = 0.0
+    Array(delta_dis, delta_en)
+
+  private def equ(dis_and_en: Array[Double], _player_input: Option[Map[String, Any]]): Array[Double] =
+    val dis = dis_and_en(0)
+    val en = dis_and_en(1)
+
+    val delta_dis = dis - Math.pow(dis, 3) / 3 - en
+    val delta_en = param.epsilon * (dis + param.alpha - param.beta * en)
+
+    Array(delta_dis, delta_en)
+  
+  private val solver = ODESolver(equation = this.equ)
+
+  def update(timestep: Double): Unit = {
+    // Calculate...
+  }
 
   def getState: MandateCurrent = state
   def getParam: MandateParam = param
